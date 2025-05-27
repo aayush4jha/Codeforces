56e9c2f947b3fc951c82639a105541fbcb710802
Aayush Jha <aayush4jha@gmail.com>
2025-05-27 21:52:23 +0530

    New update

---
+++ A_Bit.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int count=0;
+    for(int i=0;i<n;i++)
+    {
+        string s;
+        cin>>s;
+        if(s=="X++" || s=="++X")
+        {
+            count++;
+        }
+        else if(s=="X--" || s=="--X")
+        {
+            count--;
+        }
+    }
+    cout<<count;
+}
